0f274aafc3d06730135695826e8fac199d83164a
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-17 12:53:22 -0300

    feat: inicialização do projeto com Spring Boot 3.4.3, Maven e Lombok; alteração do nome do grupo de 'Dio' para 'gsmereka'

---
+++ src/main/java/gsmereka/example/api_upgrade/ApiUpgradeApplication.java
@@ -0,0 +1,13 @@
+package gsmereka.example.api_upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUpgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUpgradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gsmereka/example/api_upgrade/ApiUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package gsmereka.example.api_upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUpgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
