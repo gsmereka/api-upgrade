a478d994ec8e9a06814d8f170ba3e588700ac998
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-17 14:43:24 -0300

    feat: adicionando domain.model (Account, BaseItem, Card, Feature, Newsm User)

---
+++ src/main/java/gsmereka/example/api_upgrade/domain/model/Account.java
@@ -0,0 +1,28 @@
+package gsmereka.example.api_upgrade.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(precision = 13, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+}
---
+++ src/main/java/gsmereka/example/api_upgrade/domain/model/Card.java
@@ -0,0 +1,23 @@
+package gsmereka.example.api_upgrade.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "available_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+}
