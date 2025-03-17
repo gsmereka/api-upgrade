df9f9e8dcba57d911ba7fd9751f267fb86d6cb37
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-17 15:24:05 -0300

    feat: adicionando domain.repository.UserRepository

---
+++ src/main/java/gsmereka/example/api_upgrade/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package gsmereka.example.api_upgrade.domain.repository;
+
+import gsmereka.example.api_upgrade.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
