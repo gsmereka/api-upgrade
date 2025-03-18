a781cde0aa7d460cabf3832bd580b0794d78922c
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-17 23:26:31 -0300

    feat: adicionando controller : UserController.java, Dto de cada classe e o GlobalExceptionHandler

---
+++ src/main/java/gsmereka/example/api_upgrade/controller/dto/FeatureDto.java
@@ -0,0 +1,18 @@
+package gsmereka.example.api_upgrade.controller.dto;
+
+import gsmereka.example.api_upgrade.domain.model.Feature;
+
+public record FeatureDto(Long id, String icon, String description) {
+
+    public FeatureDto(Feature model) {
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public Feature toModel() {
+        Feature model = new Feature();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
+}
