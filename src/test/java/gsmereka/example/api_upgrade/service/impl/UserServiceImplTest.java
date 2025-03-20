afab494d7439431d8d8835d7e81c155e61505fa4
FarofaFaction <gabrielsmereka200@gmail.com>
2025-03-20 00:24:56 -0300

    feat: adicionando 2 testes de Update em UserServiceImplTest.java

--- src/test/java/gsmereka/example/api_upgrade/service/impl/UserServiceImplTest.java
+++ src/test/java/gsmereka/example/api_upgrade/service/impl/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package gsmereka.example.api_upgrade.service.impl;
 
 import gsmereka.example.api_upgrade.domain.model.User;
 import gsmereka.example.api_upgrade.domain.repository.UserRepository;
+import gsmereka.example.api_upgrade.service.exception.BusinessException;
 import gsmereka.example.api_upgrade.utils.PredefinedUser;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,7 +30,6 @@ class UserServiceImplTest {
 
     @Autowired
     UserServiceImpl userServiceImpl;
-
 
     @Test
     @DisplayName("Should find all Users successfully from DB.")
@@ -87,12 +87,99 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    @DisplayName("Should successfully create a new user.")
+    void createUserSuccessfully() {
+        User newUser = PredefinedUser.createUser();
+        newUser.setName("New User");
+        newUser.getAccount().setNumber("12345");
+        newUser.getCard().setNumber("67890");
+
+        User createdUser = userServiceImpl.create(newUser);
+
+        assertNotNull(createdUser);
+        assertEquals("New User", createdUser.getName());
+        assertEquals("12345", createdUser.getAccount().getNumber());
+        assertEquals("67890", createdUser.getCard().getNumber());
     }
 
     @Test
-    void update() {
+    @DisplayName("Should throw BusinessException when trying to create a user with null fields.")
+    void createUserWithNullFields() {
+        User newUser = null;
+
+        BusinessException thrown = assertThrows(BusinessException.class, () -> {
+            userServiceImpl.create(newUser);
+        });
+
+        assertEquals("User to create must not be null.", thrown.getMessage());
     }
+
+    @Test
+    @DisplayName("Should throw BusinessException when trying to create a user with an existing account number.")
+    void createUserWithExistingAccountNumber() {
+        User existingUser = PredefinedUser.createUser();
+        existingUser.setName("Existing User");
+        existingUser.getAccount().setNumber("12345");
+        existingUser.getCard().setNumber("67890");
+
+        userServiceImpl.create(existingUser); // Create an existing user first
+
+        User newUser = PredefinedUser.createUser();
+        newUser.setName("New User");
+        newUser.getAccount().setNumber("12345"); // Same account number as the existing one
+        newUser.getCard().setNumber("11111");
+
+        BusinessException thrown = assertThrows(BusinessException.class, () -> {
+            userServiceImpl.create(newUser);
+        });
+
+        assertEquals("This account number already exists.", thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should successfully update an existing user.")
+    void updateUserFailAtUpdateFirstUser() {
+        User initialUser = PredefinedUser.createUser();
+        initialUser.setName("Initial User");
+        initialUser.getAccount().setNumber("12345");
+        initialUser.getCard().setNumber("67890");
+
+        User createdUser = userServiceImpl.create(initialUser);
+        createdUser.setName("Updated User");
+        createdUser.getAccount().setNumber("54321");
+        createdUser.getCard().setNumber("98765");
+
+        BusinessException thrown = assertThrows(BusinessException.class, () -> {
+            User updatedUser = userServiceImpl.update(createdUser.getId(), createdUser);
+        });
+
+        assertEquals("User with ID 1 can not be updated.", thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should successfully update an existing user.")
+    void updateUserSuccessfully() {
+        User admin = PredefinedUser.createUser(); // User with ID 1 cannot be Updated, because is restricted to protect system integrity.
+        userServiceImpl.create(admin);
+
+        User initialUser = PredefinedUser.createUser();
+        initialUser.setName("Initial User");
+        initialUser.getAccount().setNumber("12345");
+        initialUser.getCard().setNumber("67890");
+
+        User createdUser = userServiceImpl.create(initialUser);
+        createdUser.setName("Updated User");
+        createdUser.getAccount().setNumber("54321");
+        createdUser.getCard().setNumber("98765");
+
+        User updatedUser = userServiceImpl.update(createdUser.getId(), createdUser);
+
+        assertNotNull(updatedUser);
+        assertEquals("Updated User", updatedUser.getName());
+        assertEquals("54321", updatedUser.getAccount().getNumber());
+        assertEquals("98765", updatedUser.getCard().getNumber());
+    }
+
 
     @Test
     void delete() {
